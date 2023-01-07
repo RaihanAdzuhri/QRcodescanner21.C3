05dd76feec5f83b9e961dfa1a8f04de089ed296e
DESKTOP-9FQ2LMA\RaihanAdzuhri <raihanadzuhri1@gmail.com>
2023-01-07 21:02:48 +0700

    5fungsi

--- app/src/main/java/com/example/qrcodescanner21c3/MainActivity.java
+++ app/src/main/java/com/example/qrcodescanner21c3/MainActivity.java
@@ -64,6 +64,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             } else if (Patterns.WEB_URL.matcher(result.getContents()).matches()) {
                 Intent visitUrl = new Intent(Intent.ACTION_VIEW, Uri.parse(result.getContents()));
                 startActivity(visitUrl);
+
+            } else if (Patterns.EMAIL_ADDRESS.matcher(result.getContents()).matches()) {
+                Intent intent = new Intent(Intent.ACTION_SEND, Uri.parse(result.getContents()));
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"raihanadzuhri1@gmail.com"});
+                intent.putExtra(Intent.EXTRA_CC, new String[] {"raihancahbekasi1@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Fungsi Email");
+                intent.putExtra(Intent.EXTRA_TEXT, "Raihan Adzuhri-TI.21.C.3-312110374");
+                try {
+                    startActivity(Intent.createChooser(intent, "Ingin Mengirim Email ?"));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    //do something else
+                }
+
             }
             else if (Patterns.PHONE.matcher(result.getContents()).matches()) {
                 String telp = String.valueOf(result.getContents());
